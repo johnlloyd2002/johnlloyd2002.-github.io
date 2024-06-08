4b614e2cceb1746a081d030d9902ee8485175909
johnlloyd2002 <llanillojohnlloyd@gmail.com>
2024-06-08 14:42:37 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}
+submit.addEventListener("click", submitComment)
